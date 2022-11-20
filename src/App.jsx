e942f3fc975dc4367521fb768672641a1f76eb20
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-11-20 18:38:10 -0500

    v1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import { useSpring, animated } from 'react-spring'
 import Player from "./player";
 
 export default function App() {
+  let [scores, setScores] = useState([])
+  let [data, setData] = useState([])
+  useEffect(() => {
+// use fetch at https://Reminisce.amukh1.repl.co/data to get all the score data
+    fetch("https://Reminisce.amukh1.repl.co/data")
+      .then(response => response.json())
+      .then(data => {
+        // Object.entries(data).sort((a,b) => b[1]-a[1])
+        setScores(Object.entries(data).sort((a,b) => b[1]-a[1]))
+        setData(data)
+      })
+  })
   return (
     <div className="App">
       <h1 className="logo">
         <em>Reminisce</em>
       </h1>
       <animated.div className="leaderboard">
-        <Player name="Amukh1" kills="26" />
-        <Player name="Socially" kills="26" />
-        <Player name="Random" kills="24" />
+        {scores.map((score, i) => {
+return <Player name={score[0].split("^")[0]} kills={score[1]} />;
+        })}
       </animated.div>
     </div>
   );
